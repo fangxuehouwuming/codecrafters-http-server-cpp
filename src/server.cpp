f82af7b98a1a65c7300f89555182347eb532433e
fangxuehouwuming <2416811040@qq.com>
2024-07-21 23:53:35 +0800

    respond with 200

--- src/server.cpp
+++ src/server.cpp
@@ -66,12 +66,12 @@ int main(int argc, char** argv) {
 
     std::cout << "Waiting for a client to connect...\n";
 
-    accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_len);
+    int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_len);
     std::cout << "Client connected\n";
 
     // respond to an HTTP request with a 200 response
     std::string response = "HTTP/1.1 200 OK\r\n\r\n";
-    send(server_fd, response.c_str(), response.size(), 0);
+    send(client_fd, response.c_str(), response.size(), 0);
 
     close(server_fd);
 
